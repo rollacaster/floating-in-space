779e7cf6f260775227805d8058f5e8af4c284acc
Thomas Sojka <thsojka@web.de>
2021-05-05 20:33:04 +0200

    Basic scene with orbit controls

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { Canvas, extend, useThree } from '@react-three/fiber'
+import { OrbitControls } from 'three-stdlib'
+
+extend({ OrbitControls })
+
 function App() {
-  return <div className=""></div>
+  return (
+    <div style={{ width: '100%', height: '100vh' }}>
+      <Canvas>
+        <Scene />
+      </Canvas>
+    </div>
+  )
+}
+
+const Scene = () => {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree()
+  return (
+    <>
+      <orbitControls args={[camera, domElement]} />
+      <mesh>
+        <boxGeometry />
+        <meshPhongMaterial color="red" />
+        <ambientLight intensity={0.4} />
+        <directionalLight position={[0, 0, 5]} />
+      </mesh>
+    </>
+  )
 }
 
 export default App
